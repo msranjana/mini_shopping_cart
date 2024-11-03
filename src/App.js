85012e68a6d59ee1f9dbb60baf781ca8dd2c46aa
Ranjana <155282747+msranjana@users.noreply.github.com>
2024-11-03 11:53:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           name: 'Antique Jhumkas', 
           price: 799, 
           image: 
-'https://simplecraftidea.com/wp-content/uploads/2020/11/1-3.jpg.webp'
+'https://aspfashionjewellery.com/cdn/shop/files/Picsart_23-06-30_15-25-59-300.jpg?v=1688211968'
         },
         { id: 2, 
           name: 'Silver Jhumkas', 
